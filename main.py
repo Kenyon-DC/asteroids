cb12e9d11af88e02af2762c65aa24032855b4292
inFamousKC <kenyondc2@gmail.com>
2025-05-12 11:14:26 -0600

    add movement and sprite groups

--- main.py
+++ main.py
@@ -2,13 +2,17 @@ import pygame
 from constants import *
 from player import Player
 
-
-
 def main():
     pygame.init()
     Clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    # Create groups
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
 
     # Instantiate Player at screen center
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
@@ -20,9 +24,10 @@ def main():
         screen.fill((0,0,0))
         
         # Update player state
-        player.update(dt)
+        updatable.update(dt)
         # Draw Player
-        player.draw(screen)
+        for sprite in drawable:
+            sprite.draw(screen)
 
         pygame.display.flip()
         dt = Clock.tick(60)/1000
